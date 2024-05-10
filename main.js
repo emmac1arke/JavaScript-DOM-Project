ddcdfb52940d2abed22d04fc9800e6b643858016
--help <clarke_emmaj@hotmail.com>
2024-05-10 14:58:57 +0100

    Made changes to JS as advised by RTC

--- main.js
+++ main.js
@@ -7,86 +7,75 @@ toggle.addEventListener('click', () => {
 })
 
 
-
-//Tag Templates for Product Cards//
-
-const productCard = `<div class="product-card">
-                <div class="image">
-                    <${image}>
-                </div>
-                <h2>${ProductName}</h2>
-                <h3>${seller}</h3>
-                <h4>${price}</h4>   
-            </div>`
-
-
 const products = [
     {ProductName: 'Jug',
     price: 14.99,
     seller: 'By the Sea',
-    image: '<https://www.houseofdisaster.com/cdn/shop/products/SEA-JUG-3_99_1000x.jpg?v=1663764035>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/products/SEA-JUG-3_99_1000x.jpg?v=1663764035'},
 
     {ProductName: 'Hut Jar with Gift Box',
     price: 12.50,
     seller: 'By the Sea',
-    image: '<https://www.houseofdisaster.com/cdn/shop/products/SEAJARHUT_1_1000x.jpg?v=1605543116>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/products/SEAJARHUT_1_1000x.jpg?v=1605543116'},
     
     {ProductName: 'Dish with Gift Box',
     price: 13.75,
     seller: 'By the Sea',
-    image: '<https://www.houseofdisaster.com/cdn/shop/products/SEA-DIS-WH-2_1000x.jpg?v=1663760936>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/products/SEA-DIS-WH-1_1000x.jpg?v=1663760936'},
     
     {ProductName: 'Tropical Wallet',
     price: 30.00,
     seller: 'Frida Kahlo',
-    image: '<https://www.houseofdisaster.com/cdn/shop/products/Fri-Wal-Tro-1_1000x.jpg?v=1634026219>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/products/Fri-Wal-Tro-1_1000x.jpg?v=1634026219'},
     
     {ProductName: 'Flask',
     price: 19.99,
     seller: 'Frida Kahlo',
-    image: '<https://www.houseofdisaster.com/cdn/shop/products/Fri-fla-1_1000x.jpg?v=1635345474>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/products/Fri-fla-1_1000x.jpg?v=1635345474'},
     
     {ProductName: 'Socks',
     price: 8.75,
     seller: 'Frida Kahlo',
-    image: '<https://www.houseofdisaster.com/cdn/shop/products/Fri-Socks-1_1000x.jpg?v=1643296918>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/products/Fri-Socks-1_1000x.jpg?v=1643296918'},
     
     {ProductName: 'Cream Cup',
     price: 19.99,
     seller: 'Forage',
-    image: '<https://www.houseofdisaster.com/cdn/shop/files/FORCUPCR-1_1000x.jpg?v=1693408534>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/files/FORCUPCR-1_1000x.jpg?v=1693408534'},
     
     {ProductName: 'Tea Light Holder',
     price: 15.99,
     seller: 'Forage',
-    image: '<https://www.houseofdisaster.com/cdn/shop/files/FORCANMU-1_1000x.jpg?v=1693408615>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/files/FORCANMU-1_1000x.jpg?v=1693408615'},
     
     {ProductName: 'Black Velvet Cushion',
     price: 42.00,
     seller: 'Forage',
-    image: '<https://www.houseofdisaster.com/cdn/shop/files/FORCUSBK-1_1000x.jpg?v=1693408395>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/files/FORCUSBK-1_1000x.jpg?v=1693408395'},
     
     {ProductName: 'Hot Water Bottle',
     price: 27.50,
     seller: 'Secret Garden',
-    image: '<https://www.houseofdisaster.com/cdn/shop/products/SEC-HWB-FX-1_1000x.jpg?v=1659805287>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/products/SEC-HWB-FX-1_1000x.jpg?v=1659805287'},
     
     {ProductName: 'Bird Planter',
     price: 29.99,
     seller: 'Secret Garden',
-    image: '<https://www.houseofdisaster.com/cdn/shop/products/SEC-PLA-BD-1_1000x.jpg?v=1658749476>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/products/SEC-PLA-BD-1_1000x.jpg?v=1658749476'},
     
     {ProductName: 'Swan Notebook',
     price: 4.99,
     seller: 'Secret Garden',
-    image: '<https://www.houseofdisaster.com/cdn/shop/products/SEC-NOT-SWA-1_1000x.jpg?v=1646650883>'},
+    image: 'https://www.houseofdisaster.com/cdn/shop/products/SEC-NOT-SWA-1_1000x.jpg?v=1646650883'},
 
 ]
 
+
+//Tag Templates for Product Cards//
 const getProductCards = (image, ProductName, seller, price) => {
     return`<div class="product-card">
 <div class="image">
-    <${image}>
+    <img src="${image}"/>
 </div>
 <h2>${ProductName}</h2>
 <h3>${seller}</h3>
